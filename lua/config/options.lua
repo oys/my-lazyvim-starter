015307652829a01042f5e25dfadce0cec14455d0
Young-Seok Oh <lynnx1029@gmail.com>
2023-10-15 00:20:55 +0900

    Disable autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.api.nvim_command("autocmd FileType go set tabstop=8 shiftwidth=8")
+vim.g.autoformat = false
