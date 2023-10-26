caae88f039c35e2837b630df96a0bd431ddf0c91
oys <lynnx1029@gmail.com>
2023-10-26 13:15:43 +0900

    JSON, XML pretty-formatting 용 단축키를 추가한다

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+map("n", "=j", "<cmd>:set modifiable<CR>:set ft=json<CR>:%!jq .<CR>", {})
+map("n", "=x", "<cmd>:set modifiable<CR>:set ft=xml<CR>:%!xmllint --format -<CR>", {})
